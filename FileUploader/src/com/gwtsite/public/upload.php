6b26527a7eca8a39e95a257b2c6da2c4d9882ec7
fongcn <fongcn@543b825f-f244-0410-a058-29c570dc5da2>
2008-02-20 00:33:10 +0000

    Initial import.

---
+++ FileUploader/src/com/gwtsite/public/upload.php
@@ -0,0 +1,16 @@
+// This code was taken from the online php manual at:
+// http://us3.php.net/features.file-upload
+<?php 
+$uploaddir = './upload';
+$uploadfile = $uploaddir . basename($_FILES['uploadFormElement']['name']);
+echo '<pre>';
+if (move_uploaded_file($_FILES['uploadFormElement']['tmp_name'], $uploadfile)) {
+    echo "File is valid, and was successfully uploaded.\n";
+} else {
+    echo "Possible file uploadhttp://us3.php.net/features.file-upload attack!\n";
+}
+
+echo 'Here is some more debugging info:';
+print_r($_FILES);
+print "</pre>";
+?>
