0aeb182ae6c97e33b4f22439510b4859dd40e48f
fongcn <fongcn@543b825f-f244-0410-a058-29c570dc5da2>
2008-03-25 15:01:23 +0000

    initial import

---
+++ trunk/Chronoscope/src/com/gwtsite/chronoscope/client/JSONRequestHandler.java
@@ -0,0 +1,9 @@
+package com.gwtsite.chronoscope.client;
+
+import com.google.gwt.core.client.JavaScriptObject;
+
+public interface JSONRequestHandler {
+	
+	public void onRequestComplete(JavaScriptObject json);
+
+}
